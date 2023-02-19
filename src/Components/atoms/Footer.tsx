a2231d43e2e0353033e72f44a2090b50bea39723
LoisotheCrow <loisocrow@gmail.com>
2023-02-19 04:44:44 +0300

    License update.

--- src/Components/atoms/Footer.tsx
+++ src/Components/atoms/Footer.tsx
@@ -3,7 +3,7 @@ import tabIndeces from "../../constants/tabIndeces";
 const Footer = () => (
   <footer tabIndex={tabIndeces.FOOTER} className="w-full h-20 bg-gray-800 flex flex-col p-4 text-gray-300">
     <div className="flex flex-row justify-between w-full">
-      <h5>Distributed under IST License</h5>
+      <h5>Distributed under MIT License</h5>
       <h5>Copyright LoisoCrow, 2023</h5>
     </div>
   </footer>
